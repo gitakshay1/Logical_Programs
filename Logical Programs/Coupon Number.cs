c86cdba91cd7daac8d106cd8620405fa737e7cf6
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-05 16:03:08 +0530

    [Akshay] Added Program for Coupon Number

---
+++ Logical Programs/Coupon Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class Coupon_Number
+    {
+        public void CouponNumber()
+        {
+            Console.WriteLine("Enter how many coupon number needed");
+            int NumCoupon=Convert.ToInt32(Console.ReadLine());
+            int Temp;
+            Console.WriteLine("Coupon Numbers are ");
+            for(int i=1;i<=NumCoupon;i++)
+            {
+                int Coupon=0;
+                Random random=new Random();
+                for(int j=1;j<=6;j++)
+                {
+                    Temp = random.Next(0, 9);
+                    Coupon = Coupon * 10 + Temp;
+                }
+                Console.WriteLine(Coupon);
+            }
+        }
+    }
+}
--- Logical Programs/Program.cs
+++ Logical Programs/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Enter Number To choose respective Program\n1 Fibonacci Series" +
-                "\n2 Perfect Number \n3 Prime Number\n4 Reverse a Number");
+                "\n2 Perfect Number \n3 Prime Number\n4 Reverse a Number\n5 Coupon Numbers");
             int Choise=Convert.ToInt32(Console.ReadLine());
             switch(Choise)
             {
@@ -24,6 +24,10 @@
                 case 4:
                     Reverse_Anumber reverse_Anumber=new Reverse_Anumber();
                     reverse_Anumber.Reverse_Number();
+                    break;
+                case 5:
+                    Coupon_Number coupon_Number=new Coupon_Number();
+                    coupon_Number.CouponNumber();
                     break;
                 default:
                     Console.WriteLine("Enter valid choise");
