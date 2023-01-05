4faf6a900efcb2c7e0b13775f35448f2fd4b27ff
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-05 14:04:30 +0530

    [Akshay] Added program for perfect number

---
+++ Logical Programs/Perfect Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class Perfect_Number
+    {
+        public void PerfectNumber()
+        {
+            Console.WriteLine("Enter Number");
+            int Number=Convert.ToInt32(Console.ReadLine());
+            int Sum = 0;
+            for(int i=1; i<=Number/2;i++)
+            {
+                if(Number%i==0)
+                {
+                    Sum += i;
+                }
+            }
+            if(Sum == Number)
+            {
+                Console.WriteLine(Number + " is Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine(Number + " is not Perfect Number");
+            }
+
+        }
+    }
+}
--- Logical Programs/Program.cs
+++ Logical Programs/Program.cs
@@ -4,13 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number To choose respective Program\n1 Fibonacci Series");
+            Console.WriteLine("Enter Number To choose respective Program\n1 Fibonacci Series" +
+                "\n2 Perfect Number ");
             int Choise=Convert.ToInt32(Console.ReadLine());
             switch(Choise)
             {
                 case 1:
                     Fibonacci_Series fibonacci_Series=new Fibonacci_Series();
                     fibonacci_Series.Fibo_Series();
+                    break;
+                case 2:
+                    Perfect_Number perfect_Number=new Perfect_Number();
+                    perfect_Number.PerfectNumber();
                     break;
                 default:
                     Console.WriteLine("Enter valid choise");
